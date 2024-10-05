02812fbc9bd324911066e106fb10e06717ed69ec
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2024-10-05 06:30:31 +0000

    datatype

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"//treat all js code as newer version
+// alert (3+3) this is not work in node js
+   //code readability should be high 
+
+   console.log("amrit")
+
+   let name = "Amrit"
+   let age = 18 
+   let isLoggedin = true;
+
+//premetive
+   // number => 2 to power 53
+   //bigint
+   //string => true/false
+   //null => standalone value  (empty)
+   //undefined => 
+    //symbol  => unique
+
+
+
+//    object 
+
+console.log(typeof undefined);
+console.log(typeof null);
+
