960fbf37063de88655f9df696913df89100b07ab
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2024-10-14 11:58:10 +0000

    oprations

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -1,9 +1,32 @@
 let score = "null"
-console.log( typeof score)
-console.log(typeof(score))
+// console.log( typeof score)
+// console.log(typeof(score))
 
 
 
 let loggValue = Number(score)
 
-console.log(loggValue)
+// console.log(loggValue)
+
+
+/*opration*/ 
+let str1 = "hello"
+let str2 = " amrit"
+ 
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2 );
+// console.log(1+ "2")
+// console.log(1+ "2"+3)
+// console.log("1"+ "2"+3)
+
+// console.log((3+4)* 5 % 3);
+
+// console.log(+true)
+// console.log(+"")
+let num1, num2, num3
+num1 = num2 =num3 =2+2
+console.log(num2)
+console.log(num1)
+console.log(num3)
