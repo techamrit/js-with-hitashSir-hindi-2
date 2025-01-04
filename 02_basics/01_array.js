fbe72dfe7a3110929ca7586a3a76bce6b88d0426
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2025-01-04 09:23:53 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,28 @@
+    //array
+
+    const myarr = [0,1,2,3,4,5]
+    const myHeros = ["saktimanj","nagraj"]
+
+    const  myArr2 = new Array(0,1,2,3,6,4,5,)
+
+    // console.log(myarr[0])
+    // myarr.push(6)
+    // myarr.push(7)
+    // myarr.unshift(8)
+    // myarr.shift()
+    // console.log(myarr)
+    // console.log(myarr.includes(8))
+    // console.log(myarr.indexOf(2))
+
+    // slice and splice
+
+    console.log("a" ,myarr);
+    const myn1 = myarr.slice(1,3)
+
+    console.log("b",myn1)
+    console.log("b",myarr)
+    const myn2 = myarr.splice(1,3)
+    
+    console.log("c",myarr)
+    console.log("c",myn2)
+    
