d8e492dfb0c5df4b846f8d1a5f00ad0cb2407ecf
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2025-01-09 07:36:40 +0000

    array 2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,29 @@
+const marvel_hero = ["iron man","caption", "thor"]
+const dc_Hero = ["super man", "flash ","batman"]
+
+// marvel_hero.push(dc_Hero)
+// console.log(marvel_hero)
+
+
+// const all_Heros = marvel_hero.concat(dc_Hero) 
+// console.log(all_Heros);
+
+// const allHeros = [...marvel_hero,...dc_Hero]
+// console.log(allHeros)
+
+const my_name =["amrit","manish","vivke",["yogi", "rahul", "amit",["iron man","caption", "thor"]]]
+
+// const my_friend = 
+
+const all_friends = my_name.flat(Infinity)
+// console.log(all_friends)
+
+console.log(Array.isArray('amrit'));
+console.log(Array.from("amrit"));
+
+const score1 =1000
+const score2 =2000
+const score3 =3000
+
+console.log(Array.of(score1,score2,score3));
+
