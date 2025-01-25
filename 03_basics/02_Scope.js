ba6dcf0e935267cb84c91035d2cb4986f3c53b2f
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2025-01-25 19:08:06 +0000

    scopes

---
+++ 03_basics/02_Scope.js
@@ -0,0 +1,13 @@
+// scope'
+ let a = 200
+
+if (true){
+    let a =10
+    const b = 20
+    console.log("inner" ,1)
+}
+ 
+ for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+ }
