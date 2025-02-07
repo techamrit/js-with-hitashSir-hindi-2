db501290355a3ca5c55aa3e3a24b88e29454193f
Amrit Pal <amritpg662@gmail.com>
2025-02-07 22:56:46 +0530

    call stack

---
+++ 03_basics/05_call_stack.js
@@ -0,0 +1,11 @@
+// javascript; execution context
+// {}global ec
+
+// javascript is singal thread
+
+// global Exaecution context
+// function Execution Context
+// eval Execution context
+// {}---> memory creation phase (is ma bas variable or baki sab ko jha di jati hai)
+
+// --->execution phase
