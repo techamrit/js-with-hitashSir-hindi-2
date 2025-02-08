56245e1d1acae0a90c75907d530312b9bd9fa721
Amrit Pal <amritpg662@gmail.com>
2025-02-08 16:30:51 +0530

    lifi

--- 03_basics/05_call_stack.js
+++ 03_basics/05_call_stack.js
@@ -9,3 +9,24 @@
 // {}---> memory creation phase (is ma bas variable or baki sab ko jha di jati hai)
 
 // --->execution phase
+// Memory creation phase
+// Execution phase
+
+// 1 globlal execution
+//  this
+
+// 2 memory phase
+//   val 1 undefined
+//   val 2 undefined
+// addnukm --> defination
+
+// result1 -->undefined
+//result2--> undefined
+
+// 3 execution phase
+// val1 10
+// val2 5
+// addNum -->  new variable environment +execution thread ---> memory phase
+// val1 --> undfefined
+// val2 --> undfefined
+// total --> undfefined
