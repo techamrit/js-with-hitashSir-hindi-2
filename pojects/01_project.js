296ec5009fc561b67cb94e20d6427c2038281870
Amrit Pal <amritpg662@gmail.com>
2025-04-21 16:29:50 +0530

    events
    eventPropagation
    1 boobling
    2 captchring
    target.parentNode
    target.id
    target.tagName

--- pojects/01_project.js
+++ pojects/01_project.js
@@ -1,80 +1,91 @@
-#projects related to DOM 
-#projectlink 
-[click hear]
+let randomNumber = parseInt(Math.random() * 100 + 1);
 
-## project 1 
-const buttons = document.querySelectorAll('.button');
-// console.log(buttons)
-const body = document.querySelector('body');
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+const startOver = document.querySelector(".resultParas");
 
-buttons.forEach(function (button) {
-  console.log(button);
+const p = document.createElement("p");
 
-  button.addEventListener('click', function (e) {
-    console.log(e);
-    console.log(e.target);
+let prevGuess = [];
+let numGuess = 1;
 
-    if (e.target.id === 'grey') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'white') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'blue') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'yellow') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'purple') {
-      body.style.backgroundColor = e.target.id;
-    }
+let playGame = true;
+
+if (playGame) {
+  submit.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    console.log(guess);
+    validateGuess(guess);
   });
-});
-
-
-```
-## project 2 solution
-
-```javascript
-const form = document.querySelector('form');
-// console.log(form)
-
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  const height = parseInt(document.querySelector('#height').value);
-  const weight = parseInt(document.querySelector('#weight').value);
-  const results = document.querySelector('#results');
-//   if (height === '' || height < 0 || isNaN(height)) {
-//     results.innerHTML = `Please give a valid height ${height}`;
-//   } else if (weight === '' || weight < 0 || isNaN(weight)) {
-//     results.innerHTML = `Please give a valid weight ${weight}`;
-//   }  else {
-//     const BMI = (weight / ((height * height) / 1000)).toFixed(2);
-//     //show the result
-//     results.innerHTML = `<span>${BMI}</span>`;
-//   }
-// });
-const bmi = (weight / ((height * height) / 1000)).toFixed(2);
-
-if (height === '' || height < 0 || isNaN(height)) {
-  results.innerHTML = `Please give a valid height ${height}`;
-} else if (weight === '' || weight < 0 || isNaN(weight)) {
-  results.innerHTML = `Please give a valid weight ${weight}`;
-} else if (bmi<=18.6){
-  //show the result
-  results.innerHTML = `<span>under weight ${bmi}</span>`;
-}
-else if (bmi>18.6&&bmi<=25){
-  //show the result
-  results.innerHTML = `<span>normal weight ${bmi}</span>`;
-}
- else if (bmi>25){
-  //show the result
-  results.innerHTML = `<span>over weight ${bmi}</span>`;
-}else{
-  results.innerHTML =` <span>wrong input</span>`
 }
 
-});
+function validateGuess(guess) {
+  if (isNaN(guess)) {
+    alert("PLease enter a valid number");
+  } else if (guess < 1) {
+    alert("PLease enter a number more than 1");
+  } else if (guess > 100) {
+    alert("PLease enter a  number less than 100");
+  } else {
+    prevGuess.push(guess);
+    if (numGuess === 11) {
+      displayGuess(guess);
+      displayMessage(`Game Over. Random number was ${randomNumber}`);
+      endGame();
+    } else {
+      displayGuess(guess);
+      checkGuess(guess);
+    }
+  }
+}
+
+function checkGuess(guess) {
+  if (guess === randomNumber) {
+    displayMessage(`You guessed it right`);
+    endGame();
+  } else if (guess < randomNumber) {
+    displayMessage(`Number is TOOO low`);
+  } else if (guess > randomNumber) {
+    displayMessage(`Number is TOOO High`);
+  }
+}
+
+function displayGuess(guess) {
+  userInput.value = "";
+  guessSlot.innerHTML += `${guess}, `;
+  numGuess++;
+  remaining.innerHTML = `${11 - numGuess} `;
+}
+
+function displayMessage(message) {
+  lowOrHi.innerHTML = `<h2>${message}</h2>`;
+}
+
+function endGame() {
+  userInput.value = "";
+  userInput.setAttribute("disabled", "");
+  p.classList.add("button");
+  p.innerHTML = `<h2 id="newGame">Start new Game</h2>`;
+  startOver.appendChild(p);
+  playGame = false;
+  newGame();
+}
+
+function newGame() {
+  const newGameButton = document.querySelector("#newGame");
+  newGameButton.addEventListener("click", function (e) {
+    randomNumber = parseInt(Math.random() * 100 + 1);
+    prevGuess = [];
+    numGuess = 1;
+    guessSlot.innerHTML = "";
+    remaining.innerHTML = `${11 - numGuess} `;
+    userInput.removeAttribute("disabled");
+    startOver.removeChild(p);
+
+    playGame = true;
+  });
+}
